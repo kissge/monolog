df06b141a68845380581ab8b31dcaf2900685bec
Yusuke Kido <mail@yusuk.eki.do>
2022-02-19 13:54:14 +0900

    (test) debug info

--- src/routes/posts.json.ts
+++ src/routes/posts.json.ts
@@ -5,7 +5,11 @@ import { config } from '../config';
 const entries = [];
 
 (async () => {
+  console.debug('Loading posts...');
+
   for (const type of await fs.promises.readdir(config.dataRootDir)) {
+    console.debug('type =', type);
+
     if (!(await fs.promises.lstat(config.dataRootDir + '/' + type)).isDirectory()) {
       continue;
     }
@@ -15,11 +19,15 @@ const entries = [];
         const body = fs.readFileSync(config.dataRootDir + '/' + type + '/' + filename, { encoding: 'utf-8' });
         const metadata = <PostMetadataParsed>yaml.safeLoad(body.split('\n--\n')[0]);
         entries.push({ ...metadata, type, slug: filename.replace(/\.md$/, '') });
+
+        console.debug(filename + ': ' + JSON.stringify(metadata));
       }
     }
   }
 
   entries.sort((a, b) => (b.date || 0) - (a.date || 0));
+
+  console.debug(entries);
 })();
 
 export const get: Sapper.ServerRoute = (_, res) => {
