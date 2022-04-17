9c811eab2d1c48d9173ea9b0f1ea29be8a0a65b3
Yusuke Kido <mail@yusuk.eki.do>
2022-04-17 22:08:07 +0900

    完全理解

--- src/routes/posts.json.ts
+++ src/routes/posts.json.ts
@@ -1,33 +1,22 @@
 import fs from 'fs';
 import { config } from '../config';
 import { parseMarkdownFile } from '../parse';
-import { debugPrint } from '../utility';
 
 const entries: (PostMetadataParsed & { type: string; slug: string })[] = [];
 
-(async () => {
-  console.debug('Loading posts...');
+console.debug('Loading posts...');
 
-  for (const type of await fs.promises.readdir(config.dataRootDir)) {
-    if ((await fs.promises.lstat(config.dataRootDir + '/' + type)).isDirectory()) {
-      for (const filename of await fs.promises.readdir(config.dataRootDir + '/' + type)) {
-        if (filename.endsWith('.md')) {
-          entries.push(parseMarkdownFile(type, filename.slice(0, -3)));
-        }
+for (const type of fs.readdirSync(config.dataRootDir)) {
+  if (fs.lstatSync(config.dataRootDir + '/' + type).isDirectory()) {
+    for (const filename of fs.readdirSync(config.dataRootDir + '/' + type)) {
+      if (filename.endsWith('.md')) {
+        entries.push(parseMarkdownFile(type, filename.slice(0, -3)));
       }
     }
   }
+}
 
-  entries.sort((a, b) => (b.date?.getTime() ?? 0) - (a.date?.getTime() ?? 0));
-
-  [
-    { t: 'head', entries: entries.slice(0, 2) },
-    { t: 'tail', entries: entries.slice(-2) },
-  ].forEach(({ t, entries }) => {
-    console.log(t);
-    entries.forEach((entry) => console.log(debugPrint(entry, null, 2)));
-  });
-})();
+entries.sort((a, b) => (b.date?.getTime() ?? 0) - (a.date?.getTime() ?? 0));
 
 export const get: Sapper.ServerRoute = (_, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' }).end(JSON.stringify(entries));
