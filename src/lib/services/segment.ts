c80e79034afb3609391395c1dcc849aa98a82f9c
Yusuke Kido <mail@yusuk.eki.do>
2022-06-08 23:59:59 +0900

    budouxのいけてないところを無理矢理後処理でなんとかする

--- src/lib/services/segment.ts
+++ src/lib/services/segment.ts
@@ -4,9 +4,13 @@ export class SegmentService {
   protected parser = loadDefaultJapaneseParser();
 
   segment(text: string) {
-    const segments = this.parser.parse(text);
+    const segments = this.parser.parse(text, 1);
     for (let i = segments.length - 1; i > 0; --i) {
-      if (['）', '？'].includes(segments[i]) || ['「'].includes(segments[i - 1])) {
+      if (
+        ['）', '？'].includes(segments[i]) ||
+        ['（', ' (', '「'].includes(segments[i - 1]) ||
+        (/^[\w:;,.!?]/.test(segments[i]) && /[\w(]$/.test(segments[i - 1]))
+      ) {
         segments[i - 1] += segments[i];
         segments.splice(i, 1);
       }
