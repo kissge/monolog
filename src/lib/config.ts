d960cf5bcd9611a98da041bca7381369d8041d3e
Yusuke Kido <mail@yusuk.eki.do>
2022-07-30 22:25:31 +0900

    Refactor

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,13 +4,13 @@ export const siteTitle = AssertionUtility.assertNonEmptyString(import.meta.env.V
 export const siteHostname = AssertionUtility.assertNonEmptyString(import.meta.env.VITE_SITE_HOSTNAME);
 export const dataRootDir = AssertionUtility.assertNonEmptyString(import.meta.env.VITE_DATA_ROOT_DIR);
 export const dataGitHubRepo = AssertionUtility.assertNonEmptyString(import.meta.env.VITE_DATA_GITHUB_REPO);
-export const dataIgnoredFiles = JSONParseAs<string[]>(import.meta.env.VITE_DATA_IGNORED_FILES, '"[]"');
-export const navLinks = JSONParseAs<{ href: string; title: string }[]>(import.meta.env.VITE_NAV_LINKS, '"[]"');
-export const topTags = JSONParseAs<string[]>(import.meta.env.VITE_TOP_TAGS, '"[]"');
+export const dataIgnoredFiles = JSONParseAs<string[]>(import.meta.env.VITE_DATA_IGNORED_FILES, []);
+export const navLinks = JSONParseAs<{ href: string; title: string }[]>(import.meta.env.VITE_NAV_LINKS, []);
+export const topTags = JSONParseAs<string[]>(import.meta.env.VITE_TOP_TAGS, []);
 export const twitterID: string | undefined = import.meta.env.VITE_TWITTER_ID;
 export const googleAnalyticsID: string | undefined = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
 export const googleAdSenseID: string | undefined = import.meta.env.VITE_GOOGLE_ADSENSE_ID;
 
-function JSONParseAs<T>(json: string, fallback: string): T {
-  return /* @__PURE__ */ JSON.parse(json || fallback);
+function JSONParseAs<T>(json: string, fallback: T): T {
+  return json ? /* @__PURE__ */ JSON.parse(json) : fallback;
 }
